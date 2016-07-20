390642de79873944dfd6f7a5db003d3ab24f1547
lucas.tomasi <lcstomasi@gmail.com>
2016-07-20 17:19:24 -0300

    Updated system

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@
 
 	angular.module( 'services' , [  'frapontillo.bootstrap-switch' , 
 									'ngRoute' ,
-									'angularUtils.directives.dirPagination' 
+									'angularUtils.directives.dirPagination'
 								 ])
 	
 	.config( [ '$routeProvider' , '$httpProvider' , 
@@ -14,29 +14,43 @@
 
 			$httpProvider.interceptors.push('Interceptor');
 			$routeProvider
-				.when('/categories', 
-				{
-					templateUrl: 'partials/categories/categories.html',
-					controller: 'CategoriesController'
 
-				})
-				.when('/category/:id', 
-				{
+/*routes categories*/
+
+				.when('/categories', {
+					templateUrl: 'partials/categories/categories.html',
+					controller: 'CategoriesController' })
+				.when('/category/:id', {
 					templateUrl: 'partials/categories/category.html',
-					controller: 'CategoryController'
-				})
-				.when('/category/', 
-				{
+					controller: 'CategoryController' })
+				.when('/category/', {
 					templateUrl: 'partials/categories/category.html',
-					controller: 'CategoryController'
-				})
+					controller: 'CategoryController' })
+
+/*routes users*/
+
+				.when('/users', {
+					templateUrl: 'partials/users/users.html',
+					controller: 'UsersController' })
+				.when('/user/:id', {
+					templateUrl: 'partials/users/user.html',
+					controller: 'UserController' })
+				.when('/user/', {
+					templateUrl: 'partials/users/user.html',
+					controller: 'UserController' })
+
+/*routes auth*/
+
 				.when('/auth', {
-					templateUrl: 'partials/auth/auth.html'
-				})
+					templateUrl: 'partials/auth/auth.html' })
+		
+/*routes defaults*/
+		
 				.when('/404', {
-					templateUrl: 'partials/404.html'
-				})
+					templateUrl: 'partials/404.html' })
 				.when('/', {
+					templateUrl: 'partials/teste.html',
+					controller: 'TesteController'
 				});
 
 			$routeProvider.otherwise({ redirectTo: '/404' });
