024a2198d670842f568c161bd653b6d765635687
lucas.tomasi <lcstomasi@gmail.com>
2016-09-25 01:04:40 -0300

    Criado datatables

---
+++ public/js/Util.js
@@ -0,0 +1,159 @@
+(function (root, factory) {
+
+    "use strict";
+
+     // CommonJS module is defined
+     if (typeof module !== 'undefined' && module.exports) {
+         module.exports = factory(require('jquery'), require('bootstrap'));
+     }
+     // AMD module is defined
+     else if (typeof define === "function" && define.amd) {
+         define("bootstrap dialog", ["jquery", "bootstrap"], function ($) {
+             return factory($);
+         });
+     } else {
+         // planted over the root!
+         root.Util = factory(root.jQuery);
+     }
+
+
+    root.Util = factory();
+}
+(this, function ($) 
+{
+	var Util = {};
+	
+	Util.hoursBetween = function( data1, data2 ) { 
+        var hours=1000*60;
+        var date1_ms = data1.getTime(); 
+        var date2_ms = data2.getTime(); 
+        var difference_ms = date2_ms - date1_ms; 
+        
+        return Math.round(difference_ms/hours); 
+    };
+
+    Util.formatDateToBR = function( date ) {
+    	
+        return moment( date ).add( 3, 'hours' ).format('DD/MM/YYYY HH:mm');
+    };
+
+    Util.getStatusReserve = function( state ) {
+        if( state == 'A' )
+        {
+            return "Waiting Accept";
+        }
+        else if( state == 'E' )
+        {
+            return "Waiting Realization";
+        }
+        else if( state == 'C' )
+        {
+            return "Rejected";
+        }
+        else if( state == 'X' )
+        {
+            return "Completed";
+        }
+        else if( state == 'Z' )
+        {
+            return "Unrealized";
+        }
+    };
+
+    Util.getIconReserve = function( state ) {
+        if( state == 'A' )
+        {
+            return "fa fa-clock-o";
+        }
+        else if( state == 'E' )
+        {
+            return "fa fa-cogs";
+        }
+        else if( state == 'C' )
+        {
+            return "fa fa-remove";
+        }
+        else if( state == 'X' )
+        {
+            return "fa fa-check";
+        }
+        else if( state == 'Z' )
+        {
+            return "fa fa-ban";
+        }
+    };
+
+    Util.getClassReserve = function( state ){
+        if( state == 'A' )
+        {
+            return "alert alert-warning";
+        }
+        else if( state == 'E' )
+        {
+            return "alert alert-info";
+        }
+        else if( state == 'C' )
+        {
+            return "alert alert-danger";
+        }
+        else if( state == 'X' )
+        {
+            return "alert alert-success";
+        }
+        else if( state == 'Z' )
+        {
+            return "alert alert-ban";
+        }
+    };
+
+    Util.generateTable = function( _id, _title ) {
+        
+        jQuery(document).ready(function(){
+            jQuery('#'+ _id +' tfoot th').each( function () {
+                var title = jQuery(this).text();
+                jQuery(this).html( '<input type="text" style="width:100%;" class="form-control" placeholder="'+title+'" />' );
+            });
+
+            var table = jQuery('#' + _id ).DataTable({
+                columnDefs: [],
+                dom: 'Bfrtip',
+                caption: { p : 'dasd' },
+                buttons: [
+                    {
+                        extend: 'pdf',
+                        text: '<i class="fa fa-file-pdf-o"></i>&nbsp;PDF',
+                        title: _title,
+                        className: 'btn btn-default'
+                    },
+                    {
+                        extend: 'excel',
+                        text: '<i class="fa fa-file-excel-o"></i>&nbsp;Excel',
+                        title: _title,
+                        className: 'btn btn-default'
+                    },
+                    {
+                        extend: 'print',
+                        text: '<i class="fa fa-print"></i>&nbsp;Print',
+                        title: _title,
+                        className: 'btn btn-default'
+                    } 
+                ]
+            });
+
+            table.columns().every( function () {
+                var that = this;
+                jQuery( 'input', this.footer() ).on( 'keyup change', function () {
+                    if ( that.search() !== this.value ) {
+                        that.search( this.value ).draw();
+                    }
+                } );
+            } );
+
+            jQuery(".dataTables_filter").remove();
+            jQuery("div.dt-buttons").append('<span class="btn" style="float:right;font-size:18px;"><b>'+_title+'</b></span>');
+
+        });
+    };
+
+    return Util;
+}));
--- public/js/controllers/users/MyRequestsController.js
+++ public/js/controllers/users/MyRequestsController.js
@@ -9,14 +9,26 @@
 		
 		function( $scope, Reservations ){
 			var user = MySession.get('user');
-			if( user ){
-				Reservations.getReservationsByUser( user.email ).success( function( reservations ){
-					console.log( reservations );
-					$scope.reservations = reservations;
+			Reservations.getReservationsByUser( user.email ).success( function( reservations ){
+				
+				$scope.reservations = reservations;
+
+				$scope.reservations.forEach( function( obj ){
+					var hour = ( Util.hoursBetween( new Date( obj.start ) , new Date( obj.end ) ) );
+					
+					obj.hours       = hour / 60 ;
+					obj.total       = parseFloat( hour * ( obj.price / 60 ) ).toFixed( 2 );
+					obj.dt_end      = Util.formatDateToBR( obj.start );
+					obj.dt_start    = Util.formatDateToBR( obj.end );
+					obj.desc_status = Util.getStatusReserve( obj.status );
+					obj.icon        = Util.getIconReserve( obj.status );
+					obj.clazz       = Util.getClassReserve( obj.status );
+					
 				});
-			} else {
-				Message.alert('Please, login' );
-			}
+
+				Util.generateTable( 'tableRequests' , 'My Requests' );
+
+			});
 	}]);
 
 })();
