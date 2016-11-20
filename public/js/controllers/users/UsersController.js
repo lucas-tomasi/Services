d48901cd91e202cf8368d5d378cc6a721788544c
lucas.tomasi <lcstomasi@gmail.com>
2016-11-20 11:37:32 -0200

    Add traduçoes e ajustado orderby

--- public/js/controllers/users/UsersController.js
+++ public/js/controllers/users/UsersController.js
@@ -7,24 +7,24 @@
 	.controller( 'UsersController' , [ '$scope' , 'UsersServices' ,
 
 		function( $scope, UsersServices )
-		{			
+		{
 			$scope.init = function()
 			{
 				$scope.columns = [
-					{ title: 'Code'     , type: 'code'       , key: '_id'      , id: '_id' },
-					{ title: 'Name'     , type: 'link'       , key: 'name'     , id: '_id' },
-					{ title: 'Login'    , type: 'link'       , key: 'login'    , id: '_id' },
-					{ title: 'E-mail'   , type: 'link'       , key: 'email'    , id: '_id' },
-					{ title: 'Provider' , type: 'link'       , key: 'provider' , id: '_id' },
-					{ title: 'Active'   , type: 'check'      , key: 'active'   , id: '_id' },
+					{ title: MyTranslate.get('CODE')     , type: 'code'       , key: '_id'      , id: '_id' },
+					{ title: MyTranslate.get('NAME')     , type: 'link'       , key: 'name'     , id: '_id' },
+					{ title: 'Login'                     , type: 'link'       , key: 'login'    , id: '_id' },
+					{ title: 'E-mail'                    , type: 'link'       , key: 'email'    , id: '_id' },
+					{ title: MyTranslate.get('PROVIDER') , type: 'link'       , key: 'provider' , id: '_id' },
+					{ title: MyTranslate.get('ACTIVE')   , type: 'check'      , key: 'active'   , id: '_id' },
 					{ title: ''         , type: 'btn-delete' , key: ''         , id: '_id' }
 				];
 
 				getUsers();
-			
-			};	
 
-			$scope.delete = function ( id ) 
+			};
+
+			$scope.delete = function ( id )
 			{
 				UsersServices.deleteUser( id ).success( function ( data ) {
 					getUsers();
@@ -40,7 +40,7 @@
 				});
 			}
 
-			$scope.init();				
+			$scope.init();
 	}]);
 
 })();
